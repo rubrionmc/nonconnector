d828e52353724a9b66b1584d7347940d15002046
leycm <leycm@proton.me>
2026-02-15 19:33:55 +0100

    feat: enhance connection handling with colored messages and implement server list ping status

--- src/main/java/net/rubrion/server/nonconnector/RubrionNonConnectorService.java
+++ src/main/java/net/rubrion/server/nonconnector/RubrionNonConnectorService.java
@@ -1,11 +1,15 @@
 package net.rubrion.server.nonconnector;
 
 import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.format.NamedTextColor;
+import net.kyori.adventure.text.format.TextColor;
 import net.minestom.server.MinecraftServer;
 import net.minestom.server.event.GlobalEventHandler;
+import net.minestom.server.event.server.ServerListPingEvent;
 import net.minestom.server.listener.manager.PacketListenerManager;
 import net.minestom.server.network.ConnectionState;
 import net.minestom.server.network.packet.client.handshake.ClientHandshakePacket;
+import net.minestom.server.ping.Status;
 
 public class RubrionNonConnectorService {
 
@@ -24,8 +28,28 @@ public class RubrionNonConnectorService {
                 return;
             }
             System.out.println(packet.getClass().getName());
-            connection.kick(Component.text("Fallback ahhhh...."));
+            if (packet.protocolVersion() >= 735) { // 1.16+
+                connection.kick(Component.text("This is the new nice color system", TextColor.color(0xFF0000)));
+                connection.disconnect();
+                return;
+            }
+            // older than 1.16
+            connection.kick(Component.text("This is only true type", NamedTextColor.DARK_PURPLE));
             connection.disconnect();
+        });
+
+        eventHandler.addListener(ServerListPingEvent.class , event -> {
+            // todo: use the common lib to build status message
+            event.setStatus(Status.builder()
+                            .versionInfo(new Status.VersionInfo("No Connection", -1))
+                            .description(Component.text("Rubrion Non-Connector Service"))
+                            .enforcesSecureChat(false)
+                            .playerInfo(Status.PlayerInfo.builder()
+                                    .maxPlayers(-1)
+                                    .onlinePlayers(0)
+                                    .sample("Your mom")
+                                    .build())
+                    .build());
         });
 
         minecraftServer.start("0.0.0.0", 25575);
